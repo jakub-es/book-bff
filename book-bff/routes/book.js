f17f76582c68abd378da20f70ed9ae2e5259d9f0
jaksta <jakub.stachura@agora.pl>
2017-03-24 14:38:36 +0100

    added goodGuy

--- book-bff/routes/book.js
+++ book-bff/routes/book.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var goodGuyLib = require('good-guy-http');
 
-/* GET home page. */
 router.get('/:isbn', function (req, res, next) {
+    var goodGuy = goodGuyLib({
+        cache: goodGuyLib.inMemoryCache(10),
+        maxRetries: 3,
+    });
+    
     var responseBody;
-    request('https://book-catalog-proxy-4.herokuapp.com/book?isbn=' + req.param('isbn'), function (error, response, body) {
+    goodGuy('https://book-catalog-proxy-4.herokuapp.com/book?isbn=' + req.param('isbn')).then(function (response) {
+
         responseBody = JSON.parse(response.body);
         var bookInfo = responseBody.items[0].volumeInfo;
         res.render('index', {
             title: bookInfo.title,
             cover: bookInfo.imageLinks.thumbnail
         });
-    });
+    }).catch(next);
 });
 
 module.exports = router;
-
-
